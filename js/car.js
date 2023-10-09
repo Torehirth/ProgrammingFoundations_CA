b1653d22aae26346acb2d635ee1b8307745e5a0a
Tore Mæland Hirth <torehirth@gmail.com>
2023-10-09 22:04:09 +0200

    add object of car

---
+++ js/car.js
@@ -0,0 +1,51 @@
+const car = {
+  type: {
+    brand: "volkswagen",
+    model: "golf",
+  },
+  engine: {
+    fuel: "diesel",
+    CC: 2000,
+    hk: 150,
+  },
+  km: 200000,
+  gearbox: "dsg",
+  gears: [
+    {
+      name: "park",
+    },
+    {
+      name: "neutral",
+      ratio: 0,
+    },
+    {
+      name: "first",
+      ratio: 1.1,
+    },
+    {
+      name: "second",
+      ratio: 1.2,
+    },
+    {
+      name: "third",
+      ratio: 1.3,
+    },
+    {
+      name: "fourth",
+      ratio: 1.4,
+    },
+    {
+      name: "fifth",
+      ratio: 1.5,
+    },
+    {
+      name: "sixth",
+      ratio: 1.6,
+    },
+  ],
+  revs: 0,
+  speed: 0,
+  on: false,
+  // function
+  // ignition: function () {}
+};
