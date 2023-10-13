5e4d82640f35caca12cdaea4d32595f1b22e2685
Tore Mæland Hirth <torehirth@gmail.com>
2023-10-13 13:37:33 +0200

    resized screenprint readme file

--- js/car.js
+++ js/car.js
@@ -1,3 +1,6 @@
+// The object below consists of nested objects, arrays of objects and a method/function.
+// The object has different property values (string, number and bolean).
+
 const car = {
   type: {
     brand: "volkswagen",
@@ -47,25 +50,40 @@ const car = {
   revs: 0,
   speed: 0,
   on: false,
-  // function:
   ignition: function () {
     this.on = !this.on;
+    // This method/function inverts the bolean value of the object property "on".
+    // Calling the function turns the ignition on when it's off and vice versa.
   },
 };
 
-// Transfer brand and model to html.
+// Calling the ignition function to change the bolean value of the "on" property of the "car" object.
+// Below is an example that calling the function shifts the bolean value(view in console).
+car.ignition();
+console.log("ignition status: " + car.on); //true
+car.ignition();
+console.log("ignition status: " + car.on); //false
+
+// -------------------------------------------------------------------------------
+
+// Transfer different property values to html sheet.
+
+// Transfering brand and model to html.
 const brandHeadline = document.querySelector("#brand");
 const modelHeadline = document.querySelector("#model");
 
 brandHeadline.innerHTML = car.type.brand;
 modelHeadline.innerHTML = car.type.model;
 
-// Transfer gear and gear ratio to html when buttons on page is clicked.
+console.log("Car brand and model: " + car.type.brand, car.type.model);
 
 // Transfer object values to HUD in html.
 const engineSpecs = document.querySelector("#engine");
 
 engineSpecs.innerHTML += " " + car.engine.cc + "<p>CC</p>" + " , " + car.engine.hp + "<p>HP</p>" + " , " + car.engine.fuel;
+
+console.log("Engine property values: " + car.engine.cc, car.engine.hp, car.engine.fuel);
+console.log("Engine fuel value type: " + typeof car.engine.fuel);
 
 //inital values.
 const revolutions = document.querySelector("#revs");
@@ -74,19 +92,24 @@ const speed = document.querySelector("#speed");
 revolutions.innerHTML += " " + car.revs + "<p>RPM</p>";
 speed.innerHTML += " " + car.speed + "<p>Km/h</p>";
 
+console.log("engine revs: " + car.revs);
+
 const odometer = document.querySelector("#km");
 odometer.innerHTML += " " + car.odometer + "<p>Km</p>";
 
-// Transfer car ignition value to HUD.
-// const carIgnition = document.querySelector("#ignitionstatus");
-// carIgnition.innerHTML += " " + car.on;
+console.log("car travel: " + car.odometer + "km");
 
-// Gear buttons onclick.
-// Ignition button function.
+// ------------------------------------------------------------------------------
 
+// "Start" button toggle function.
+// When "start" button is toggled the values of html elements below is simultaneous changed.  
+ 
+// Elements in html sheet I want to change by toggle/clicking "start" button.
 const toggleIgnition = document.getElementById("ignitionbtn");
 const ignitionStatus = document.getElementById("ignitionstatus");
 const engineRevs = document.getElementById("revs");
+
+console.log(typeof toggleIgnition);
 
 toggleIgnition.addEventListener("click", function () {
   car.ignition();
@@ -97,8 +120,10 @@ toggleIgnition.addEventListener("click", function () {
 function changeIgnitionStatus() {
   if (car.on === true) {
     ignitionStatus.textContent = "Ignition ON";
+    console.log("Ignition ON");
   } else {
     ignitionStatus.textContent = "Ignition OFF";
+    console.log("Ignition OFF");
   }
 }
 
@@ -109,3 +134,9 @@ function changeEngineRevs() {
     engineRevs.textContent = "Revs: 0RPM";
   }
 }
+
+console.log(typeof car.ignition);
+
+//--------------------------------------------------------------------------------
+
+// Transfering gear and gear ratio to html when buttons on page is clicked.
